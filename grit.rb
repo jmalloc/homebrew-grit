ad42ba074b78d5a0985fe083b68b0270ef6bd6d5
James Harris <mailjamesharris@gmail.com>
2020-12-10 07:40:39 +1000

    Update to v

--- grit.rb
+++ grit.rb
@@ -2,9 +2,9 @@ class Grit < Formula
   desc "Keep track of your local Git clones."
   homepage "https://github.com/jmalloc/grit"
 
-  version "0.6.9"
-  url "https://github.com/jmalloc/grit/releases/download/0.6.9/grit-darwin-amd64.tar.gz"
-  sha256 "21396fe326dd5af15f626cb63e77c0003bfb6774ed260c59951414fe45168ae3"
+  version ""
+  url "https://github.com/jmalloc/grit/releases/download//grit--darwin-amd64.zip"
+  sha256 "31c479e2b861c72097536dfc751fea05bcd01c09d42f86d70e5d9a4732390339"
 
   def install
       bin.install "grit"
