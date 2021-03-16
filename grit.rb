373cf9058583196b017ff30be98bffd4491e4e1f
Travis CI User <travis@example.org>
2021-03-16 23:29:20 +0000

    Update to v0.7.0

--- grit.rb
+++ grit.rb
@@ -4,7 +4,7 @@ class Grit < Formula
 
   version "0.7.0"
   url "https://github.com/jmalloc/grit/releases/download/0.7.0/grit-0.7.0-darwin-amd64.zip"
-  sha256 "ef9e179f0d87cb7be9ac1617ff085de85f371f6d728260c966f966e5f39132a8"
+  sha256 "948029c8f76b387a6bc1e5664fa8b4b6afe2eb705d731ce955c1f3e78b45c99a"
 
   def install
       bin.install "grit"
