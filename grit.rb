1c86fba94a2738c90adbb111fe180e5fb1eaa364
James Harris <contact@jamesharris.id.au>
2024-10-21 08:47:25 +1000

    Bump v0.7.4.

--- grit.rb
+++ grit.rb
@@ -2,17 +2,17 @@ class Grit < Formula
   desc "Keep track of your local Git clones."
   homepage "https://github.com/jmalloc/grit"
 
-  version "0.7.3"
+  version "0.7.4"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/jmalloc/grit/releases/download/0.7.3/grit-0.7.3-darwin-amd64.zip"
-      sha256 "d3189e173980402d4ff8328bd9a6b2a78c75487cbbc37238cbbc90550beb1a0c"
+      url "https://github.com/jmalloc/grit/releases/download/0.7.4/grit-0.7.4-darwin-amd64.zip"
+      sha256 "da72c9f2daff3d3202ed56118a4dc6298ef0feea6661abccc78dee34e48ce74a"
     end
 
     if Hardware::CPU.arm?
-      url "https://github.com/jmalloc/grit/releases/download/0.7.3/grit-0.7.3-darwin-arm64.zip"
-      sha256 "6c72daf50b22770a172675b2455d138b98667055d3e13c816edd89fe742dadd7"
+      url "https://github.com/jmalloc/grit/releases/download/0.7.4/grit-0.7.4-darwin-arm64.zip"
+      sha256 "01753109b88a4381b279dd95762704408b4aac48d853e263c0c911721f8a91fe"
     end
   end
 
