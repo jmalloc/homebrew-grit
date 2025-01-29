c227e7d3c890cd4a8d0fc8d14c0d0a46969c12a1
James Harris <contact@jamesharris.id.au>
2025-01-29 10:38:23 +1000

    Add missing `do` keywords.

--- grit.rb
+++ grit.rb
@@ -5,12 +5,12 @@ class Grit < Formula
   version "0.7.5"
 
   on_macos do
-    on_arm
+    on_arm do
       url "https://github.com/jmalloc/grit/releases/download/0.7.5/grit-0.7.5-darwin-arm64.zip"
       sha256 "05c141a37dfd44721783362ab43045124a4434e7a4e6a0fe6310aa6cf3182061"
     end
 
-    on_intel
+    on_intel do
       url "https://github.com/jmalloc/grit/releases/download/0.7.5/grit-0.7.5-darwin-amd64.zip"
       sha256 "55db17eb6c0b41a1967618b1ecf6eec21bb2a0609827a175bbecd87087cb403e"
     end
