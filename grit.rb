8908cfea9f0e2a110a9e8fd796681798a63225de
James Harris <contact@jamesharris.id.au>
2025-01-29 10:01:13 +1000

    Fix archive hashes.

--- grit.rb
+++ grit.rb
@@ -7,12 +7,12 @@ class Grit < Formula
   on_macos do
     if Hardware::CPU.intel?
       url "https://github.com/jmalloc/grit/releases/download/0.7.5/grit-0.7.5-darwin-amd64.zip"
-      sha256 "037212deca2d916a7184470a5816611277dcf87c548c7180a3f4942e839049d7"
+      sha256 "55db17eb6c0b41a1967618b1ecf6eec21bb2a0609827a175bbecd87087cb403e"
     end
 
     if Hardware::CPU.arm?
       url "https://github.com/jmalloc/grit/releases/download/0.7.5/grit-0.7.5-darwin-arm64.zip"
-      sha256 "ef934bf711bc580a10862528d622b74fdddd2a6ad864f6f9d04c992542a2e70c"
+      sha256 "05c141a37dfd44721783362ab43045124a4434e7a4e6a0fe6310aa6cf3182061"
     end
   end
 
